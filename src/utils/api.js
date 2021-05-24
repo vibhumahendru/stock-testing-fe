5261a0d57204889271d0bb74c346a106241104f8
vibhumahendru <vibhumahendru@gmail.com>
2021-05-24 18:11:34 +0530

    new

--- src/utils/api.js
+++ src/utils/api.js
@@ -9,7 +9,8 @@ const prod = {
 };
 const dev = {
  url: {
-  API_URL: 'http://127.0.0.1:8000'
+  // API_URL: 'http://127.0.0.1:8000'
+  API_URL: 'https://backtest-portfolio.herokuapp.com/'
  }
 };
 
